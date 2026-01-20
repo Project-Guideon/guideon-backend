494f14ccd419ff0c556613a76e272b4d46340457
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2026-01-20 20:00:48 +0900

    Init: Spring Boot 초기 세팅

---
+++ src/main/groovy/com/guideon/guideonbackend/GuideonBackendApplication.groovy
@@ -0,0 +1,13 @@
+package com.guideon.guideonbackend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GuideonBackendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GuideonBackendApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/guideon/guideonbackend/GuideonBackendApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.guideon.guideonbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GuideonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
