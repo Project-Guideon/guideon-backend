cf3e2508405861b5491939e9dfb49d6758565944
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2026-01-25 17:10:32 +0900

    refactor: migrate from Groovy to Java 21 and fix build dependencies

---
+++ src/main/java/com/guideon/guideonbackend/GuideonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.guideon.guideonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuideonBackendApplication.class, args);
+    }
+
+}
