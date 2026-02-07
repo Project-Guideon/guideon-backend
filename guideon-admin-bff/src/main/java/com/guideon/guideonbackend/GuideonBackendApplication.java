0d7871440cf500659f3e5035355a3b3eecd972b8
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2026-02-08 02:34:11 +0900

    build: amdin-bff Spring Cloud OpenFeign 의존성 추가(#35)

--- guideon-admin-bff/src/main/java/com/guideon/guideonbackend/GuideonBackendApplication.java
+++ guideon-admin-bff/src/main/java/com/guideon/guideonbackend/GuideonBackendApplication.java
@@ -3,6 +3,7 @@ package com.guideon.guideonbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -15,6 +16,7 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableJpaRepositories(basePackages = "com.guideon.core.domain")
 @EnableRedisRepositories(basePackages = "com.guideon.core.domain")
 @EnableJpaAuditing
+@EnableFeignClients(basePackages = "com.guideon.guideonbackend.client")
 public class GuideonBackendApplication {
 
     public static void main(String[] args) {
