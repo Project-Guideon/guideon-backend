bae5161a967e8c2ffaec93d18657b5b98addada8
TaeYeon Hwang <ty5321@naver.com>
2026-02-20 23:30:18 +0900

    feat(bff):  재처리 Feign Client 추가(#53)

--- guideon-admin-bff/src/main/java/com/guideon/guideonbackend/client/CoreDocumentClient.java
+++ guideon-admin-bff/src/main/java/com/guideon/guideonbackend/client/CoreDocumentClient.java
@@ -2,6 +2,7 @@ package com.guideon.guideonbackend.client;
 
 import com.guideon.core.dto.CreateDocumentCommand;
 import com.guideon.core.dto.DocumentDto;
+import com.guideon.core.dto.ReprocessDocumentCommand;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +27,12 @@ public interface CoreDocumentClient {
     DocumentDto getDocument(
             @PathVariable("siteId") Long siteId,
             @PathVariable("docId") Long docId);
+
+    @PostMapping("/internal/v1/sites/{siteId}/documents/{docId}/reprocess")
+    DocumentDto reprocessDocument(
+            @PathVariable("siteId") Long siteId,
+            @PathVariable("docId") Long docId,
+            @RequestBody ReprocessDocumentCommand command);
 
     @DeleteMapping("/internal/v1/sites/{siteId}/documents/{docId}")
     void deleteDocument(
