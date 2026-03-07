84f68130649f5a9faf700c2a096994138d81d36b
TaeYeon Hwang <ty5321@naver.com>
2026-03-08 02:22:14 +0900

    feat: @EnableFeignClients 추가
    -Feign 인테페이스 사용

--- guideon-core/src/main/java/com/guideon/core/GuideonCoreApplication.java
+++ guideon-core/src/main/java/com/guideon/core/GuideonCoreApplication.java
@@ -2,6 +2,7 @@ package com.guideon.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  * Docker 환경에서 Core가 별도 서비스로 실행될 때 사용
  */
 @EnableAsync
+@EnableFeignClients(basePackages = "com.guideon.core.client")
 @SpringBootApplication(scanBasePackages = {
         "com.guideon.core",
         "com.guideon.common"
