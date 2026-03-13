98a36e966daa8530d3ff49ad711de14abe5ff012
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2026-03-13 10:32:43 +0900

    feat: enable HTTPS with SSL certificate

--- guideon-admin-bff/src/main/java/com/guideon/guideonbackend/global/config/SecurityConfig.java
+++ guideon-admin-bff/src/main/java/com/guideon/guideonbackend/global/config/SecurityConfig.java
@@ -122,6 +122,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
+
     }
 
     /**
