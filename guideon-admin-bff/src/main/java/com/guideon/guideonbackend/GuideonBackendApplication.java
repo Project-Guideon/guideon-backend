0bc5372b857fbd6238c56e5abdb69bb97bb07154
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2026-03-19 01:13:09 +0900

    feat: 이메일   발송 서비스 및 초대 템플릿 구현 (#99)

--- guideon-admin-bff/src/main/java/com/guideon/guideonbackend/GuideonBackendApplication.java
+++ guideon-admin-bff/src/main/java/com/guideon/guideonbackend/GuideonBackendApplication.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication(scanBasePackages = {
         "com.guideon.guideonbackend",
         "com.guideon.core.domain"
