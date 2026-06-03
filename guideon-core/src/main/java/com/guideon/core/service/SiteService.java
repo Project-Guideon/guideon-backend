f051a3d16a30902d422638cdf0e34f048fc31e49
TaeYeon Hwang <ty5321@naver.com>
2026-06-03 22:37:02 +0900

    feat: Site 생성·수정 API에 지도 좌표 필드 추가 (#185)
    - CreateSiteRequest/UpdateSiteRequest: 좌표 필드 + 범위 검증(위도 ±90, 경도 ±180, 줌 1~14)
    - SiteResponse: 좌표 필드 응답 포함
    - SiteService: Core로 좌표 전달

--- guideon-core/src/main/java/com/guideon/core/service/SiteService.java
+++ guideon-core/src/main/java/com/guideon/core/service/SiteService.java
@@ -33,6 +33,9 @@ public class SiteService {
     public SiteDto createSite(CreateSiteCommand command) {
         Site site = Site.builder()
                 .name(command.getName())
+                .latitude(command.getLatitude())
+                .longitude(command.getLongitude())
+                .mapLevel(command.getMapLevel())
                 .build();
 
         siteRepository.save(site);
@@ -65,6 +68,7 @@ public class SiteService {
     public SiteDto updateSite(Long siteId, UpdateSiteCommand command) {
         Site site = findSiteById(siteId);
         site.updateName(command.getName());
+        site.updateMapLocation(command.getLatitude(), command.getLongitude(), command.getMapLevel());
         log.info("관광지 수정 완료: siteId={}, name={}", site.getSiteId(), site.getName());
 
         return SiteDto.from(site);
